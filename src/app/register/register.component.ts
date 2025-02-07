72734dbc85f813b845d02ea9ea32e244a7b55d16
Jhonata Escatolini <jescatolini@gmail.com>
2025-02-07 01:36:08 -0300

    register simple ts

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  username = '';
+  password = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  register() {
+    this.authService.register(this.username, this.password).subscribe(() => {
+      alert('Cadastro realizado com sucesso!');
+      this.router.navigate(['/login']);
+    }, error => {
+      alert('Erro no cadastro: ' + error.error.message);
+    });
+  }
 }
